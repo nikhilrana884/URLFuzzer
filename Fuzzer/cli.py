c2d265aac8ba5dcdfe9dc14d8a2f6bb1042c501a
nikhilrana884 <nikhilrana884@gmail.com>
2022-01-25 20:42:17 +0530

    Fuzzer Python files

---
+++ Fuzzer/cli.py
@@ -0,0 +1,166 @@
+import sys
+import time
+import argparse
+from pythonping import ping
+import socket
+from pwnlib import *
+from colored import fg, attr
+
+from .actions import RangeAction
+from .actions import UrlAction
+from .actions import DictAction
+from .actions import ListAction
+from .actions import DataAction
+
+
+class ZFuzzCLI(object):
+
+    def __init__(self):
+
+
+        self.log = log.getLogger('pwnlib.exploit')
+        log.install_default_handler()
+
+        self.bold = attr("bold")
+        self.red = fg(203)
+        self.green = fg(77)
+        self.blue = fg(69)
+        self.magenta = fg(170) + self.bold
+        self.default = attr("reset")
+
+    def print_banner(self):
+
+
+        bannerstr = f"""     ___
+      |  _|_ _ ___ ___
+      |  _| | |- _|- _|
+      |_| |___|___|___| {self.default + self.bold}MINI PROJECT GRP-3
+                     """
+        print(self.magenta + bannerstr + self.default)
+
+
+
+    def print_help(self):
+
+        helpstr = f"""{self.bold}Usage: {sys.argv[0]} [OPTIONS] [WORDLIST] [URL]
+\nZFuzz Options:{self.default}
+    [-h/--help]      -- Print this help message
+    [-u/--url]       -- URL to fuzz
+    [-w/--wordlist]  -- wordlist
+    [-H/--headers]   -- HTTP headers
+    [-d/--data]      -- POST data
+    [-X/--verb]      -- HTTP verb. Default get
+    [-b/--cookies]   -- Cookie to send
+    [-k/--keyword]   -- Fuzzing keyword. Default ^FUZZ^
+    [-t/--threads]   -- Number of threads. Default 35
+    [-s/--delay]     -- Delay between requests
+    [-r/--follow]    -- Follow HTTP redirection
+    [--quiet]        -- Do not print additional information
+    [--timeout]      -- Requests timeout
+    [--hc/sc]        -- HTTP Code(s) to hide/show
+    [--hs/ss]        -- Response to hide/show that match with the given str
+    [--hr/sr]        -- Response to hide/show that match with the given regex
+    [--hl/sl]        -- Response lenght to hide/show
+    """
+
+        self.print_banner()
+        print(helpstr)
+
+    def parse_args(self, argv):
+
+
+
+        parser = argparse.ArgumentParser(add_help=False,
+                                         description="Python Web Fuzzer")
+
+        parser.add_argument("-t", "--threads",
+                            type=int, default=35, action=RangeAction,
+                            mini=0, maxi=100)
+
+        parser.add_argument("-w", "--wordlist",
+                            type=argparse.FileType('r', errors='ignore'),
+                            required=True)
+
+        parser.add_argument("-u", "--url",
+                            type=str, action=UrlAction, required=True)
+
+        parser.add_argument("-H", "--headers",
+                            type=str, default={}, nargs='*', action=DictAction)
+
+        parser.add_argument("-d", "--data",
+                            type=str, default={}, action=DataAction)
+
+        parser.add_argument("-X", "--verb",
+                            choices=["GET", "HEAD", "POST", "OPTIONS", "PUT"],
+                            type=str, default="get")
+
+        parser.add_argument("-b", "--cookies",
+                            type=str, default={}, nargs='*', action=DictAction)
+
+        parser.add_argument("-k", "--keyword",
+                            type=str, default="^FUZZ^")
+
+        parser.add_argument("-s", "--delay",
+                            type=float, default=0)
+
+        parser.add_argument("-r", "--follow",
+                            action="store_true")
+
+        parser.add_argument("--quiet",
+                            action="store_true")
+
+        parser.add_argument("--timeout", type=float)
+
+        parser.add_argument("--hc",
+                            type=str, default=[], action=ListAction)
+
+        parser.add_argument("--sc",
+                            type=str, default=[], action=ListAction)
+
+        parser.add_argument("--hs", type=str)
+
+        parser.add_argument("--ss", type=str)
+
+        parser.add_argument("--hr", type=str)
+
+        parser.add_argument("--sr", type=str)
+
+        parser.add_argument("--hl", type=int)
+
+        parser.add_argument("--sl", type=int)
+
+        return parser.parse_args(argv)
+
+    def main(self, argv):
+
+        from zfuzz.fuzzer import Fuzz
+
+        if len(argv) <= 1 or "--help" in argv or "-h" in argv:
+            self.print_help()
+            sys.exit(1)
+
+        args = self.parse_args(argv[1:])
+
+        if not args.quiet:
+            self.print_banner()
+            old_time = time.time()
+            self.log.info("Target: {}".format(args.url.replace("^FUZZ^","<fuzz>")))
+            print()
+
+        try:
+            Fuzz(**vars(args))
+        except KeyboardInterrupt:
+            sys.exit(1)
+
+        if not args.quiet:
+            new_time = time.time()
+            print()
+
+            print(f'host name of the machine is {socket.gethostname()}')
+            print(f'IP address of the machine is {socket.gethostbyname(socket.gethostname())}')
+            ping('google.com',verbose=True)
+
+
+
+            self.log.success(f"Scan completed successfully in {int(new_time - old_time)}s")
+            print()
---
+++ Fuzzer/fuzzer.py
@@ -0,0 +1,121 @@
+import threading
+import sys
+import time
+import queue
+import requests
+
+from .utils import replace_kv_dict
+from .utils import get_code_color
+from .utils import is_matching
+from .cli import ZFuzzCLI
+
+
+class Fuzz(object):
+
+    """ Used to fuzz an host with multi-threads
+
+        :param url: Url to fuzz
+        :param wordlist: Wordlist used
+        :param headers: HTTP Headers
+        :param data: POST Data
+        :param verb: HTTP verb
+        :param cookies: HTTP Cookies
+        :param threads: Threads numbers
+        :param keyword: Fuzzing keyword to use
+        :param timeout: Requests timeout
+        :param delay: Delay between requests
+        :param follow: Follow HTTP Requests
+        :param quiet: Do not print additional information
+        :param hc: HTTP Code(s) to hide
+        :param sc: HTTP Code(s) to show
+        :param hs: Hide reponse with the given str
+        :param ss: Show reponse with the given str
+        :param hr: Hide response with the given regex
+        :param sr: Show response with the given regex
+        :param hl: Response lenght to hide
+        :param sl: Response lenght to show
+    """
+
+    def __init__(self, url, wordlist, headers, data, verb, cookies,
+                 threads, keyword, timeout, delay, follow, quiet,
+                 hc, sc, hs, ss, hr, sr, hl, sl):
+
+        self.cli = ZFuzzCLI()
+        self.log = self.cli.log
+
+        self._url = url
+        self._wordlist = wordlist
+        self._headers = headers
+        self._data = data
+        self._cookies = cookies
+        self._threads = threads
+        self._keyword = keyword
+        self._timeout = timeout
+        self._delay = delay
+        self._follow = follow
+        self._quiet = quiet
+        self._hc = hc
+        self._sc = sc
+        self._hs = hs
+        self._ss = ss
+        self._hr = hr
+        self._sr = sr
+        self._hl = hl
+        self._sl = sl
+
+        self._method = (requests.post if data and verb.lower() == "get"
+                        else eval("requests." + verb.lower()))
+
+        self.run()
+
+    def fuzz(self, i, q):
+
+
+
+        while True:
+            i = q.get()
+            link = self._url.replace(self._keyword, i)
+            headers = replace_kv_dict(self._headers.copy(), self._keyword, i)
+            data = replace_kv_dict(self._data.copy(), self._keyword, i)
+            cookies = replace_kv_dict(self._cookies.copy(), self._keyword, i)
+            try:
+                res = self._method(link, headers=headers, data=data,
+                                   cookies=cookies, timeout=self._timeout,
+                                   allow_redirects=self._follow)
+
+                time.sleep(self._delay)
+                code = res.status_code
+                if is_matching(code, self._hc, self._sc, str(res.content),
+                               len(res.text), self._hs, self._ss, self._hr,
+                               self._sr, self._hl, self._sl):
+
+                    color = get_code_color(code)
+                    if not self._quiet:
+                        state = (f"[{color}{code}{self.cli.default}]: {i}\n")
+                        self.log.warn(state)
+                    else:
+                        sys.stdout.write(i + '\n')
+
+            except Exception as e:
+                print(e)
+
+            finally:
+                q.task_done()
+
+    def run(self):
+
+
+        q = queue.Queue()
+
+        lines = self._wordlist.read().splitlines()
+        for line in lines:
+            q.put(line.rstrip('\n\r'))
+
+        threads = [threading.Thread(target=self.fuzz, args=(i, q))
+                   for i in range(int(self._threads))]
+
+        for t in threads:
+            t.setDaemon(True)
+            t.start()
+
+        q.join()
